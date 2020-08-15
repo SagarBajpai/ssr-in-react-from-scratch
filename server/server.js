920dcf20b3eb8bba774b87c454ae33ef5b9f0051
sagarbajpai <ashversache@gmail.com>
2020-08-15 23:28:18 +0530

    babel setup in express server

--- server/server.js
+++ server/server.js
@@ -1,11 +1,15 @@
-const express = require("express");
+import express from "express";
+import ReactDOMServer from "react-dom/server";
 
+import AppComponent from "./../src/App";
+import React from "react";
 const app = express();
 
 app.get("**", (req, res) => {
-  res.send("Hello Response");
+  const content = ReactDOMServer.renderToString(<AppComponent />);
+  res.send(content);
 });
 
-app.listen(4000, () => {
-  console.log("Listening to Port 4000");
+app.listen(4001, () => {
+  console.log("Listening to Port 4001");
 });
